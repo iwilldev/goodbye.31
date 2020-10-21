e01bfecd0d92d3c82accdd571463ef06baf05201
William Gonçalves <euwilliamgoncalves@gmail.com>
2020-10-21 16:25:07 -0300

    Finalizando terceiro projeto

---
+++ 03 - Calculadora com JavaScript/script.js
@@ -0,0 +1,28 @@
+const screen = document.querySelector('textarea');
+let isLastInputANumber = false;
+let isResultDisplayed = false;
+screen.value = '';
+
+function inputOperator(key) {
+  if (isLastInputANumber) {
+    screen.value += key;
+  }
+  isLastInputANumber = false;
+  isResultDisplayed = false;
+}
+
+function inputNumber(key) {
+  if (isResultDisplayed) {
+    screen.value = key;
+    isResultDisplayed = false;
+    isLastInputANumber = true;
+  } else {
+    screen.value += key;
+    isLastInputANumber = true;
+  }
+}
+
+function displayResult() {
+  screen.value = math.evaluate(calc.txt.value);
+  isResultDisplayed = true;
+}
