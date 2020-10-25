e759ec8a73991252e53f409a407f9eb2fa6cd81a
William Gonçalves <euwilliamgoncalves@gmail.com>
2020-10-24 22:20:16 -0300

    Finalizando sexto projeto

---
+++ 06 - Pomodoro Times com JavaScript/assets/js/script.js
@@ -0,0 +1,90 @@
+let startButton = document.querySelector('main .container .button-container .start');
+let pauseButton = document.querySelector('main .container .button-container .pause');
+let resetButton = document.querySelector('main .container .button-container .reset');
+
+let focusMinutes = document.querySelector('main .container .focus-timer .focus-minutes');
+let focusSeconds = document.querySelector('main .container .focus-timer .focus-seconds');
+let focusInput = document.querySelector('main .container #focus-input');
+let focusInitialTime = focusInput.value;
+focusMinutes.innerText = focusInitialTime;
+
+let breakMinutes = document.querySelector('main .container .break-timer .break-minutes');
+let breakSeconds = document.querySelector('main .container .break-timer .break-seconds');
+let breakInput = document.querySelector('main .container #break-input');
+let breakInitialTime = breakInput.value;
+breakMinutes.innerText = breakInitialTime;
+
+let startTimer;
+
+startButton.addEventListener('pointerdown', () => {
+  if(startTimer === undefined) {
+    startTimer = setInterval(timer, 1000);
+  } else {
+    alert('O timer já está ativo');
+  }
+  startButton.removeEventListener('pointerdown', this);
+})
+
+resetButton.addEventListener('pointerdown', () => {
+  focusMinutes.innerText = focusInitialTime;
+  focusSeconds.innerText = "00";
+    
+  breakMinutes.innerText = breakInitialTime;
+  breakSeconds.innerText = "00";
+
+  document.querySelector('main .container #cycles .cycles-counter').innerText = 0;
+  stopInterval();
+  startTimer = undefined;
+  resetButton.removeEventListener('pointerdown', this);
+})
+
+pauseButton.addEventListener('pointerdown', () => {
+  stopInterval();
+  startTimer = undefined;
+  pauseButton.removeEventListener('pointerdown', this);
+})
+
+focusInput.addEventListener('change', function() {
+  focusInitialTime = focusInput.value;
+  focusMinutes.innerText = focusInitialTime;
+  focusInput.removeEventListener('change', this);
+})
+
+breakInput.addEventListener('change', function() {
+  breakInitialTime = breakInput.value;
+  breakMinutes.innerText = breakInitialTime;
+  breakInput.removeEventListener('change', this);
+})
+
+function timer() {
+
+  if(focusSeconds.innerText != 0){
+    focusSeconds.innerText--;
+  } else if(focusMinutes.innerText != 0 && focusSeconds.innerText == 0) {
+    focusSeconds.innerText = 59;
+    focusMinutes.innerText--;
+  }
+
+  if(focusMinutes.innerText == 0 && focusSeconds.innerText == 0) {
+    if(breakSeconds.innerText != 0){
+      breakSeconds.innerText--;
+    } else if(breakMinutes.innerText != 0 && breakSeconds.innerText == 0) {
+      breakSeconds.innerText = 59;
+      breakMinutes.innerText--;
+    }
+  }
+
+  if(focusMinutes.innerText == 0 && focusSeconds.innerText == 0 && breakMinutes.innerText == 0 && breakSeconds.innerText == 0) {
+    focusMinutes.innerText = focusInitialTime;
+    focusSeconds.innerText = "00";
+    
+    breakMinutes.innerText = breakInitialTime;
+    breakSeconds.innerText = "00";
+
+    document.querySelector('main .container #cycles .cycles-counter').innerText++;
+  }
+}
+
+function stopInterval() {
+  clearInterval(startTimer);
+}
